c1e5c1a7661037ed1f3af117f6be49c17adf8fc9
HectorGbravo <launchx04896@innovaccion.mx>
2023-06-10 17:44:38 -0600

    Creado postwork 4

---
+++ postwork_4/src/test/java/org/bedu/postwork_1/Postwork1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.postwork_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Postwork1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
